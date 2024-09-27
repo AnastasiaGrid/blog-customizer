d1bd7cd9a854a0db9230953662f85ccf9c8c97c0
Anastasia Gridneva <anastasia.gridneva@mail.ru>
2024-09-27 14:32:42 +0300

    Update project after review

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { defaultArticleState, IFormData } from 'src/constants/articleProps';
 import { ArticleParamsForm } from 'src/components/article-params-form/ArticleParamsForm';
@@ -7,12 +7,15 @@ import { Article } from 'src/components/article/Article';
 import styles from '../styles/index.module.scss';
 
 export const App = () => {
-	const inLocalStorage =
-		localStorage.getItem('form') !== null
-			? JSON.parse(localStorage.form)
-			: defaultArticleState;
+	const [state, setState] = useState<IFormData>(defaultArticleState);
+	useEffect(() => {
+		setState(
+			localStorage.getItem('form') !== null
+				? JSON.parse(localStorage.form)
+				: defaultArticleState
+		);
+	}, []);
 
-	const [state, setState] = useState<IFormData>(inLocalStorage);
 	const handleUpdate = (formData: IFormData) => {
 		setState(formData);
 	};
